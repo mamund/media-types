a44afcb913c00e242eef15f20ac6cd9e0a98cf0f
Mike Amundsen <mamund@yahoo.com>
2014-03-04 10:43:22 -0500

    fix troublesome <data> example representation

--- includes/search-sample.js
+++ includes/search-sample.js
@@ -4,47 +4,42 @@
     "version" : "1.0", 
     "data" :
     [
-      {
-        "id" : "root", 
-        "data" :
-        [
-          {"rel" : "self", "url" : "http://example.org/"},
-          {
-            "name" : "list", 
-            "rel" : "collection", 
-            "url" : "http://example.org/list/",
-            "data" :
-            [
-              {
-                "name" : "search", 
-                "rel" : "search",
-                "url" : "http://example.org/search",
-                "model" : "{&title}"
-              },
-              {
-                "name" : "todo",
-                "rel" : "item",
-                "url" : "http://example.org/list/1",
-                "data" : 
-                [
-                  {"name" : "title", "value" : "Clean house"},
-                  {"name" : "dueDate", "value" : "2014-05-01"}
-                ]
-              },
-              {
-                "name" : "todo",
-                "rel" : "item",
-                "url" : "http://example.org/list/2",
-                "data" : 
-                [
-                  {"name" : "title", "value" : "Paint the fence"},
-                  {"name" : "dueDate", "value" : "2014-06-01"}
-                ]
-              }
-            ]  
-          }
-        ]
-      }    
-    ]
+      {"rel" : "self", "url" : "http://example.org/"},
+        {
+          "name" : "list", 
+          "rel" : "collection", 
+          "url" : "http://example.org/list/",
+          "data" :
+          [
+            {
+              "name" : "search", 
+              "rel" : "search",
+              "url" : "http://example.org/search",
+              "model" : "{&title}"
+            },
+            {
+              "name" : "todo",
+              "rel" : "item",
+              "url" : "http://example.org/list/1",
+              "data" : 
+              [
+                {"name" : "title", "value" : "Clean house"},
+                {"name" : "dueDate", "value" : "2014-05-01"}
+              ]
+            },
+            {
+              "name" : "todo",
+              "rel" : "item",
+              "url" : "http://example.org/list/2",
+              "data" : 
+              [
+                {"name" : "title", "value" : "Paint the fence"},
+                {"name" : "dueDate", "value" : "2014-06-01"}
+              ]
+            }
+          ]  
+        }
+      }
+    ]    
   }
 }
